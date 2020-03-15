f3945c1e46cd60f54fa788805e79a037529f753e
Melvin <melvinalmonte@gmail.com>
2020-03-15 14:49:24 -0400

    changed .next.config removed serverless

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 const withSass = require("@zeit/next-sass");
 
 module.exports = {
-  ...withSass(),
-  target: "serverless",
-  webpack: config => {
-    config.node = {
-      fs: "empty"
-    };
-    return config;
-  }
+  ...withSass()
 };
