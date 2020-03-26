35540e6df749937b535564baf29189f9a2b8d3dc
Melvin <melvinalmonte@gmail.com>
2020-03-25 21:34:59 -0400

    changed update tag placement

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,18 +1,18 @@
 const Footer = props => {
   const { lastUpdated } = props;
-  const date = new Date(lastUpdated)
-  const lastUpdate = date.toString()
+  const date = new Date(lastUpdated);
+  const lastUpdate = date.toString();
   return (
     <footer className="footer">
       <div className="content has-text-centered">
+        <p>
+          This data was last updated <code>{lastUpdate}</code>
+        </p>
         <p>
           Built with data from <strong>mathdroid </strong>{" "}
           <a href="https://github.com/mathdroid/covid-19-api" target="_blank">
             COVID-19 API
           </a>
-        </p>
-        <p>
-          Data presented as of <code>{lastUpdate}</code>
         </p>
       </div>
     </footer>
