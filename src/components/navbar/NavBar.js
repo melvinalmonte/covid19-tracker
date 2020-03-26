98c9f83e53bc5192172c116342563f7d4d1abb3f
Melvin <melvinalmonte@gmail.com>
2020-03-26 00:01:40 -0400

    changed header color and position

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,10 +1,8 @@
 const NavBar = () => {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <div className="navbar-item">
-          <h1>COVID-19 Tracker</h1>
-        </div>
+      <div className="column is-12 has-text-centered">
+        <h1 className="title has-text-info">COVID-19 Tracker</h1>
       </div>
     </nav>
   );
